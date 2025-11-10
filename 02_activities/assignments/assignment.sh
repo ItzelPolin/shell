7e56aac5752afd7541f43be7d43e259e0c86d8ec
Itzel <itzel@@gmail.com>
2025-11-10 16:26:45 -0500

    New  answers at 4

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -52,12 +52,12 @@ mkdir  server_logs user_logs event_logs
 cd ..
 ##cp ./raw/server*   ./processed/server_logs
 ##cp ./raw/*.log   ./processed/server_logs
-cp   .data/raw/*server*.log   .data/processed/server_logs/
+cp   ./raw/*server*.log   ./processed/server_logs/
 
 
 # 6. Repeat the above step for user logs and event logs
-cp  .data/raw/*user*.log   .data/processed/user_logs/
-cp  .data/raw/*event*.log  .data/processed/event_logs/
+cp  ./raw/*user*.log   ./processed/user_logs/
+cp  ./raw/*event*.log  ./processed/event_logs/
 ##cp ./raw/user*  ./processed/user_logs
 ##cp ./raw/event*  ./processed/event_logs
 
@@ -65,7 +65,7 @@ cp  .data/raw/*event*.log  .data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 ##rm .raw/user_ipaddr*
-rm processed/user_logs/*ipaddr* ./data/raw/*ipaddr*
+rm ./processed/user_logs/*ipaddr* ./raw/*ipaddr*
  
 
 
