8fdb35b4414633e86e0e42cbc8da4439a698ec35
Itzel <itzel@@gmail.com>
2025-11-10 14:28:24 -0500

    New  answers at 14:28

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -49,20 +49,23 @@ mkdir  server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-cp ./raw/server*   ./processed/server_logs
-cp ./raw/*.log   ./processed/server_logs
+##cp ./raw/server*   ./processed/server_logs
+##cp ./raw/*.log   ./processed/server_logs
+cp   raw/*server*.log   processed/server_logs/
 
 
 # 6. Repeat the above step for user logs and event logs
-cp ./raw/user*  ./processed/user_logs
-cp ./raw/event*  ./processed/event_logs
+cp  raw/*user*.log   processed/server_logs/
+cp   raw/*event*.log   processed/server_logs/
+##cp ./raw/user*  ./processed/user_logs
+##cp ./raw/event*  ./processed/event_logs
 
 
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm .raw/user_ipaddr*
-rm ./processed/user_logs/user_ipaddr*
-rm .raw/ipaddr*
+##rm .raw/user_ipaddr*
+rm ./processed/user_logs/*ipaddr*
+rm .raw/*ipaddr*
 
 
 
